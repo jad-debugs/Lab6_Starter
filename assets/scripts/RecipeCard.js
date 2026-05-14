90fb4b1f4f16f0fa9fbbf89577970c0bd4dc7534
Jad Isaac <jadjrisaac@gmail.com>
2026-05-13 23:38:11 -0700

    fix end bracket

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -89,7 +89,9 @@ class RecipeCard extends HTMLElement {
 		time {
 			color: #70757A;
 			font-size: 12px;
+        }
         `;
+
 	    // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
         shadow.append(style, article);
 	}
